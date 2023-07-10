410978e1ffa278f3acaa2ef5ed5c8f6908b8f7fe
Higo-99 <114218896+Higo-99@users.noreply.github.com>
2023-07-10 15:58:28 +0700

    add image into homepage

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const viewEngine = (app) => {
-    app.use(express.static("./src/public"));
+    // app.use(express.static("./src/public"));
+    // app.set("views", "./src/views")
+    app.use(express.static(path.join('./src/public')));
+    app.set('views', path.join('./src/views'));
     app.set("view engine", "ejs");
-    app.set("views", "./src/views")
 }
 
 module.exports = viewEngine
