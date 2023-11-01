ce1884336ef88781ac395ee1c45fa1f87d4282f0
Higo-99 <114218896+Higo-99@users.noreply.github.com>
2023-11-01 11:19:07 +0700

    Change model product: fragrance, introduce, style

--- Frontend/src/Test/Test.js
+++ Frontend/src/Test/Test.js
@@ -4,25 +4,6 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { fragranceList } from './ProductSelectOptions';
 
 const Test = () => {
-    const myArray = [
-        { id: 1, name: 'Object 1' },
-        { id: 2, name: 'Object 2' },
-        { id: 3, name: 'Object 3' }
-    ];
-
-    const idsToFind = [1, 3];
-    const nameToFind = ['Object 1', 'Object 2'];
-
-    const foundObjects = myArray.filter(item => nameToFind.includes(item.name));
-
-    if (foundObjects.length > 0) {
-        // Objects with the specified ids were found
-        console.log('Found Objects:', foundObjects);
-    } else {
-        // Objects with the specified ids were not found
-        console.log('Objects not found');
-    }
-
     const frgs2 = [fragranceList[1], fragranceList[3]];
 
     const savedfrags = ["Akigalawood", "Amberwood"];
